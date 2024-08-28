0de69e544bf8b1530cd3cef18e632c4b15af0c27
dev-faizal <134308892+dev-faizal@users.noreply.github.com>
2024-08-28 20:07:11 +0300

    Add files via upload

---
+++ Calculator/calculator.js
@@ -0,0 +1,21 @@
+let result = document.getElementById("result");
+
+function appendNumber(num) {
+  result.value += num;
+}
+
+function appendOperator(operator) {
+  result.value += operator;
+}
+
+function calculate() {
+  try {
+    result.value = eval(result.value);
+  } catch (error) {
+    result.value = "Error";
+  }
+}
+
+function clearResult() {
+  result.value = "";
+}
